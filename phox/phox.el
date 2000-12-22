99236eb46f265bc4eced266a2ab6c235ba71dfea
crr <crr>
2000-12-22 14:03:10 +0000

    *** empty log message ***

--- phox/phox.el
+++ phox/phox.el
@@ -142,7 +142,7 @@ phox-comments-regexp
    (concat
     "^"
     phox-comments-regexp
-    "\\(constraints\\|flag\\|goals\\|pri\\(nt\\(_sort\\)?\\|ority\\)\\|eshow\\|search\\|depend\\)")
+    "\\(constraints\\|flag\\|goals\\|print\\(_sort\\)?\\|eshow\\|search\\|priority\\|depend\\)")
    proof-non-undoables-regexp       
    (concat
     "^"
